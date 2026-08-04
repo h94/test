fb81ae296d1275ed6a7b9e316df27dec8872a00e
cai <b4400440077@gmail.com>
2026-08-04 10:36:09 +0800

    color

--- app/app.vue
+++ app/app.vue
@@ -6,7 +6,7 @@
       <div class="pa-5 h-100">
         <NuxtPage />
       </div>
-      11
+      1122
     </v-main>
   </v-app>
 </template>
@@ -23,6 +23,10 @@ body {
   height: 100vh;
   width: 100vw;
 }
-
+main {
+  height: 100vh;
+  overflow: hidden;
+  color: #000;
+}
 </style>
 
